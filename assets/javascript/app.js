597b968c2be9f2ecc41379a471d74f7c74d93299
Marlo Bennett <marlomariebennett@gmail.com>
2016-08-23 17:32:41 -0500

    updated js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,24 @@
 //put all question vars in an array
-var questions = [question1, question2, question3, question4];
+//var questions = [question1, question2, question3, question4];
 
 //put each question in a variable (all necessary html elements)
-var question1 = "<p>Which of the following animals are Hogwarts students not allowed to bring to school as a pet?</p> <input type='radio' name='q1' value='incorrect'>Cat<br /> <input type='radio' name='q1' value='incorrect'>Rat<br /> <input type='radio' name='q1' value='correct'>Bat<br /> <input type='radio' name='q1' value='incorrect'>Owl";
+var question1 = "<div id='firstQuestion'><p>Which of the following animals are Hogwarts students not allowed to bring to school as a pet?</p> <ul id='trivia'> <li> <input type='radio' name='ques' value='incorrect'> Cat </li> <li> <input type='radio' name='ques' value='incorrect'> Rat</li> <li> <input type='radio' name='ques' value='correct' id='yes'> Bat</li> <li><input type='radio' name='ques' value='incorrect'> Owl </li> </ul></div>";
 
-var question2 = "<p>Which magical creature rescued Harry, Hermione, Ron, Luna, Dean Thomas, and Mr. Olivander from the Malfoys' basement?</p> <input type='radio' name='q2' value='correct'>Dobby the house elf<br /> <input type='radio' name='q2' value='incorrect'>Winky the house elf<br /> <input type='radio' name='q2' value='incorrect'>Buckbeak the thestral<br /> <input type='radio' name='q2' value='incorrect'>The giant squid";
+var question2 = "<div id='secondQuestion'><p>Which magical creature rescued Harry, Hermione, Ron, Luna, Dean Thomas, and Mr. Olivander from the Malfoys' basement?</p> <input type='radio' name='q2' value='correct'>Dobby the house elf<br /> <input type='radio' name='q2' value='incorrect'>Winky the house elf<br /> <input type='radio' name='q2' value='incorrect'>Buckbeak the thestral<br /> <input type='radio' name='q2' value='incorrect'>The giant squid</div>";
 
-var question3 = "<p>What play opened in London this year and continues the story of Harry, Hermione, and Ron as adults?</p> <input type='radio' name='q3' value='incorrect'>Harry Potter and the Magic of Middle Age<br /> <input type='radio' name='q3' value='incorrect'>Harry Potter and the Chosen Child<br /> <input type='radio' name='q3' value='incorrect'>Harry Potter and the Society for the Promotion of Elfish Welfare<br /> <input type='radio' name='q3' value='correct'>Harry Potter and Cursed Child";
+var question3 = "<div id='thirdQuestion'><p>What play opened in London this year and continues the story of Harry, Hermione, and Ron as adults?</p> <input type='radio' name='q3' value='incorrect'>Harry Potter and the Magic of Middle Age<br /> <input type='radio' name='q3' value='incorrect'>Harry Potter and the Chosen Child<br /> <input type='radio' name='q3' value='incorrect'>Harry Potter and the Society for the Promotion of Elfish Welfare<br /> <input type='radio' name='q3' value='correct'>Harry Potter and Cursed Child</div>";
 
-var question4 = "<p>Which deathy hallow did Harry acquire first?</p> <input type='radio' name='q4' value='correct'>The cloak of invisibility<br /> <input type='radio' name='q4' value='incorrect'>The resurrection stone<br /> <input type='radio' name='q4' value='incorrect'>The elder wand<br /> <input type='radio' name='q4' value='incorrect'>The vial of felix felicis";
+var question4 = "<div id='fourthQuestion'><p>Which deathy hallow did Harry acquire first?</p> <input type='radio' name='q4' value='correct'>The cloak of invisibility<br /> <input type='radio' name='q4' value='incorrect'>The resurrection stone<br /> <input type='radio' name='q4' value='incorrect'>The elder wand<br /> <input type='radio' name='q4' value='incorrect'>The vial of felix felicis</div>";
 
 var correctAnswers;
 var incorrectAnswers;
 var unansweredQuestions;
+
+$(document).ready(function() {
+
+initializeNewGame();
+
+showQuestions();
 
 function initializeNewGame() {
 
@@ -21,16 +27,27 @@ function initializeNewGame() {
  	unansweredQuestions = 0;
 }
 
-
-//giant for loop to loop through and pull in each question
-for (q = 0; q < questions.length; q++) {
-	showQuestions();
-}
-
 function showQuestions() {
 
+	if (correctAnswers + incorrectAnswers + unansweredQuestions === 0) {
+		$("#game").append(question1);
+	}
 
+	$("#trivia").on("click", function(event) {
+		var answerChoice = $("#yes").checked;
+		if (answerChoice) {
+			correctAnswers++;
+			$("#game").append("<div id='youreCorrect'><h5>That's right!</h5></div>")
+		}
+	})
 }
+
+
+
+
+
+
+})
 //each question must:
 //start the timer
 //on click (radio buttons) to determine which answer has been selected. 
